cff8ffd0dda98885d18e0989fdcd7c234b265cf9
blablupcom <o.volodin@yahoo.com>
2018-03-03 00:50:20 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -39,13 +39,13 @@ def validateFilename(filename):
 
 def validateURL(url):
     #try:
-        r = requests.post(url, data = datadict, allow_redirects=True, stream=True, headers=user_agent)
+        r = requests.post(url, data = datadict, allow_redirects=True, proxies=proxy)
         print r.text
         count = 1
         while r.status_code >= 500 and count < 4:
             print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
             count += 1
-            r = requests.post(url, data=datadict, allow_redirects=True, stream=True, headers=user_agent)
+            r = requests.post(url, data = datadict, allow_redirects=True, proxies=proxy)
         sourceFilename = r.headers.get('Content-Disposition')
         if sourceFilename:
             ext = os.path.splitext(sourceFilename)[1].replace('"', '').replace(';', '').replace(' ', '')
